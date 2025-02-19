1dde786f1102077476ff63beb7c60039981a3a79
rudi221 <r.dreibach@t-online.de>
2025-02-19 11:13:53 +0100

    UI Anpassungen

--- Kuma/Controls/ucMenu.Designer.cs
+++ Kuma/Controls/ucMenu.Designer.cs
@@ -28,18 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnAddArtist = new Button();
+            btnDeleteArtist = new Button();
+            btnSendData = new Button();
+            button1 = new Button();
+            btnAddArtistFile = new Button();
             SuspendLayout();
+            // 
+            // btnAddArtist
+            // 
+            btnAddArtist.Image = Properties.Resources.User58;
+            btnAddArtist.Location = new Point(13, 13);
+            btnAddArtist.Name = "btnAddArtist";
+            btnAddArtist.Size = new Size(85, 85);
+            btnAddArtist.TabIndex = 0;
+            btnAddArtist.Text = "Neu";
+            btnAddArtist.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnAddArtist.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteArtist
+            // 
+            btnDeleteArtist.Image = Properties.Resources.userDelete58;
+            btnDeleteArtist.Location = new Point(109, 13);
+            btnDeleteArtist.Name = "btnDeleteArtist";
+            btnDeleteArtist.Size = new Size(85, 85);
+            btnDeleteArtist.TabIndex = 1;
+            btnDeleteArtist.Text = "Löschen";
+            btnDeleteArtist.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnDeleteArtist.UseVisualStyleBackColor = true;
+            // 
+            // btnSendData
+            // 
+            btnSendData.Image = Properties.Resources.Mail58;
+            btnSendData.Location = new Point(1055, 13);
+            btnSendData.Name = "btnSendData";
+            btnSendData.Size = new Size(85, 85);
+            btnSendData.TabIndex = 2;
+            btnSendData.Text = "Versenden";
+            btnSendData.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnSendData.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Image = Properties.Resources.FolderDelete58;
+            button1.Location = new Point(959, 13);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 85);
+            button1.TabIndex = 3;
+            button1.Text = "Löschen";
+            button1.TextImageRelation = TextImageRelation.ImageAboveText;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // btnAddArtistFile
+            // 
+            btnAddArtistFile.Image = Properties.Resources.FolderAdd58;
+            btnAddArtistFile.Location = new Point(861, 13);
+            btnAddArtistFile.Name = "btnAddArtistFile";
+            btnAddArtistFile.Size = new Size(85, 85);
+            btnAddArtistFile.TabIndex = 4;
+            btnAddArtistFile.Text = "Neu";
+            btnAddArtistFile.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnAddArtistFile.UseVisualStyleBackColor = true;
             // 
             // UcMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
+            Controls.Add(btnAddArtistFile);
+            Controls.Add(button1);
+            Controls.Add(btnSendData);
+            Controls.Add(btnDeleteArtist);
+            Controls.Add(btnAddArtist);
             Name = "UcMenu";
             Size = new Size(1154, 113);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnAddArtist;
+        private Button btnDeleteArtist;
+        private Button btnSendData;
+        private Button button1;
+        private Button btnAddArtistFile;
     }
 }
